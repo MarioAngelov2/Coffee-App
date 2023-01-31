20f65b8132e0355f87ae91159f58f847b8f0f94e
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2023-01-31 19:22:26 +0200

    Add css to footer

---
+++ app/src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../styles/Footer.css'
+
+function Footer() {
+  return (
+    <div className='footer'>
+      <p> &copy; 2023 Coffe Shop </p>
+    </div>
+  )
+}
+
+export default Footer
