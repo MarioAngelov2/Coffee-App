0c2e93675a0dd0b6112e7c87194bc55e052e3bcc
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2023-01-31 19:56:26 +0200

    Add css to home page

--- app/src/pages/Home.jsx
+++ app/src/pages/Home.jsx
@@ -2,14 +2,17 @@ import React from "react";
 
 import { Link } from "react-router-dom";
 import homeImage from "../assets/coffee-banner.jpg";
+import "../styles/Home.css";
 
 function Home() {
   return (
     <div className="home" style={{ backgroundImage: `url(${homeImage})` }}>
-      <div className="headerContainer">The Best Coffee Ever</div>
+      <div className="headerContainer">
+        <h1>The Best Coffee Ever</h1>
       <Link to="/catalog">
         <button>ORDER NOW</button>
       </Link>
+      </div>
     </div>
   );
 }
