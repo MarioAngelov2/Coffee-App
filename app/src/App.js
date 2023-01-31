d3a7875aa6241cb94b276002d625bf5ac52d78c7
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2023-02-01 00:00:46 +0200

    Update App.js

--- app/src/App.js
+++ app/src/App.js
@@ -6,6 +6,7 @@ import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Home from "./pages/Home";
 import Catalog from "./pages/Catalog";
+import Login from "./pages/Login";
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
       <Routes>
         <Route path="/" exact element={<Home />} />
         <Route path="/catalog" exact element={<Catalog />} />
+        <Route path="/login" exact element={<Login />} />
       </Routes>
       <Footer />
     </div>
