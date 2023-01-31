bcb13a5f27858889bca3aff16451b78f0019fe80
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2023-01-31 20:09:24 +0200

    Create helper json for catalog page

---
+++ app/src/helpers/catalogList.js
@@ -0,0 +1,27 @@
+import MochaCoffee from "../assets/mocha-coffee.webp";
+import MochaFrappe from "../assets/mocha-frappe.webp";
+import IcedLatte from "../assets/iced-latte.webp";
+import FrozenCoffe from "../assets/frozen-coffee.webp";
+
+export const catalogList = [
+  {
+    name: "Mocha",
+    image: MochaCoffee,
+    price: 4.99,
+  },
+  {
+    name: "Mocha Frappe",
+    image: MochaFrappe,
+    price: 6.99,
+  },
+  {
+    name: "Iced Latte",
+    image: IcedLatte,
+    price: 5.99,
+  },
+  {
+    name: "Frozen Coffe",
+    image: FrozenCoffe,
+    price: 7.99,
+  },
+];
