2ad150ea50a1b4a517964a7b3ca0ddea5729274c
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2023-01-31 20:50:23 +0200

    Add css to catalog page & catalog items

--- app/src/App.js
+++ app/src/App.js
@@ -5,6 +5,7 @@ import { Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Home from "./pages/Home";
+import Catalog from "./pages/Catalog";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" exact element={<Home />} />
+        <Route path="/catalog" exact element={<Catalog />} />
       </Routes>
       <Footer />
     </div>
---
+++ app/src/pages/Catalog.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+import { catalogList } from "../helpers/catalogList";
+import CatalogItem from "../components/CatalogItem";
+import '../styles/Catalog.css';
+
+function Catalog() {
+  return <div className="menu">
+    <h1 className="catalogTitle">Our Products</h1>
+    <div className="catalogList">
+      {catalogList.map((catalogItem, key) => {
+        return (
+          <CatalogItem 
+            key={key}
+            image={catalogItem.image}
+            name={catalogItem.name}
+            price={catalogItem.price}
+          />
+        )
+      })}
+    </div>
+  </div>;
+}
+
+export default Catalog;
