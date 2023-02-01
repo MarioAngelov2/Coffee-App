9269a0f88ce4ab0da075f203fba2342ac79c9b2a
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2023-02-01 21:58:38 +0200

    Add css to register page

--- app/src/pages/Register.jsx
+++ app/src/pages/Register.jsx
@@ -1,26 +1,27 @@
 import React from "react";
+import "../styles/Register.css";
 
 function Register() {
   return (
     <div className="register">
       <h1>Register</h1>
-      <form id="register-form" method="POST">
-        <label htmlFor="username">Username:</label>
-        <input name="username" placeholder="Enter username..." type="email" />
-        <label htmlFor="password">Password:</label>
-        <input
-          name="password"
-          placeholder="Enter your password..."
-          type="password"
-        />
-        <label htmlFor="re-password">Repeat Password:</label>
-        <input
-          name="re-password"
-          placeholder="Repeat password..."
-          type="password"
-        />
-        <button type="submit">Register</button>
-      </form>
+        <form id="register-form" method="POST">
+          <label htmlFor="username">Username:</label>
+          <input name="username" placeholder="Enter username..." type="email" />
+          <label htmlFor="password">Password:</label>
+          <input
+            name="password"
+            placeholder="Enter your password..."
+            type="password"
+          />
+          <label htmlFor="re-password">Repeat Password:</label>
+          <input
+            name="re-password"
+            placeholder="Repeat password..."
+            type="password"
+          />
+          <button type="submit">Register</button>
+        </form>
     </div>
   );
 }
