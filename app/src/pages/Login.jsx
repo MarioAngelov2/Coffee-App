40836f62385a2295f7312c5d0f61c56b2a527a3a
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2023-02-01 20:51:06 +0200

    Add css to login page

--- app/src/pages/Login.jsx
+++ app/src/pages/Login.jsx
@@ -1,16 +1,17 @@
 import React from "react";
 import "../styles/Login.css";
+import loginImage from '../assets/loginImage.jpeg';
 
 function Login() {
   return (
     <div className="loginContainer">
-      <div className="leftSide"></div>
+      <div className="leftSide" style={{ backgroundImage: `url(${loginImage})` }}></div>
       <div className="rightSide">
         <h1>Login</h1>
         <form id="login-form" method="POST">
-          <label htmlFor="username">Username</label>
+          <label htmlFor="username">Username:</label>
           <input name="username" placeholder="Enter username..." type="email" />
-          <label htmlFor="password">Password</label>
+          <label htmlFor="password">Password:</label>
           <input
             name="password"
             placeholder="Enter your password..."
