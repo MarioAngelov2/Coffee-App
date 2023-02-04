5d93c69150153f0dff5b67831fb091b197e860d6
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2023-02-04 10:20:15 +0200

    Update details

--- app/src/pages/Details.jsx
+++ app/src/pages/Details.jsx
@@ -1,11 +1,27 @@
 import React from "react";
+import CatalogItem from "../components/CatalogItem";
+import { catalogList } from "../helpers/catalogList";
+import '../styles/Details.css';
 
 function Details() {
   return (
     <div className="details">
-      <p>text</p>
+      <div className="itemDetails">
+        {
+          catalogList.map((itemDetail, key) => {
+            return(
+              <CatalogItem 
+              key={key}
+              image={itemDetail.image}
+              name={itemDetail.name}
+              price={itemDetail.price}
+              />
+            )
+          })
+        }
+      </div>
     </div>
-  )
+  );
 }
 
 export default Details;
