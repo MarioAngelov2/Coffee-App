38ff79c129a9ef1b0241c663253cf6ddeba0c127
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2023-03-06 23:39:08 +0200

    Update CatalogItem.jsx

--- app/src/components/CatalogItem.jsx
+++ app/src/components/CatalogItem.jsx
@@ -1,11 +1,10 @@
 import React, { useContext } from "react";
-import { useNavigate, Link } from 'react-router-dom';
+import { useNavigate, Link } from "react-router-dom";
 
-function CatalogItem({coffee}) {
-
+function CatalogItem({ coffee }) {
   // let navigate = useNavigate();
   // onClick={() => {navigate(`/details/${id}`)}}
- 
+
   return (
     <div className="catalogItem">
       <div style={{ backgroundImage: `url(${coffee.image})` }}></div>
