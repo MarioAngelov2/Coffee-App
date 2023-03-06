e066d3e41de5bc34c98f6d33842c32e2ee03c737
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2023-03-06 23:39:15 +0200

    Update Catalog.jsx

--- app/src/pages/Catalog.jsx
+++ app/src/pages/Catalog.jsx
@@ -1,16 +1,28 @@
 import React from "react";
+import { useState, useEffect } from "react";
 
+import { getAll } from "../services/getService";
 import { catalogList } from "../helpers/catalogList";
 import CatalogItem from "../components/CatalogItem";
 import "../styles/Catalog.css";
 
-function Catalog({ coffeInfo }) {
+function Catalog({ }) {
+  const [item, setItem] = useState([]);
+
+  useEffect(() => {
+    getAll()
+      .then(result => {
+        setItem(result)
+      })
+  }, 
+  []);
+
   return (
     <div className="menu">
       <h1 className="catalogTitle">Our Products</h1>
       <div className="catalogList">
-        {coffeInfo.map((item) => {
-          return <CatalogItem key={item.id} coffee={item} />;
+        {Object.values(item).map((item) => {
+          return <CatalogItem key={item._id} coffee={item} />;
         })}
       </div>
     </div>
