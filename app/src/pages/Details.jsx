627a2ab882408184e1385e0f6d90c04ba79c5ec7
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2023-03-06 20:51:48 +0200

    Update Details.jsx

--- app/src/pages/Details.jsx
+++ app/src/pages/Details.jsx
@@ -3,11 +3,14 @@ import { useParams } from "react-router-dom";
 import "../styles/Details.css";
 
 function Details({ coffeInfo }) {
+  const { id } = useParams();
+  console.log('coffeeId:', id);
   console.log('coffeInfo', coffeInfo)
-  const { coffeeId } = useParams();
 
-  const coffee = coffeInfo.find(x => x.id === coffeeId);
-  console.log('coffee', coffee)
+
+  // const coffee = Object.values(coffeInfo).find((x) => x.id === id);
+  const coffee = coffeInfo.find((coffee) => coffee.id === id);
+  // console.log('coffee', coffee)
 
   return (
     <div className="details">
