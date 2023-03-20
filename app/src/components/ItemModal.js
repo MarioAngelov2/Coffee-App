d257c87eafa81fdef1f5313c0325ced1506016e4
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2023-03-20 20:53:55 +0200

    Create delete function and filter the state

--- app/src/components/ItemModal.js
+++ app/src/components/ItemModal.js
@@ -1,12 +1,29 @@
-import React from "react";
+import React, { useState, useEffect, useContext } from "react";
+import { deleteItem } from "../services/deleteService";
+import { useNavigate } from "react-router-dom";
+import { CatalogStateContext } from "../context/DataContext";
+
 import "../styles/ItemModal.css";
+
 import Button from "@mui/material/Button";
 import CloseIcon from "@mui/icons-material/Close";
+import DeleteIcon from "@mui/icons-material/Delete";
 import Stack from "@mui/material/Stack";
 import { ThemeProvider } from "@mui/material/styles";
 import theme from "../theme/theme";
 
 function ItemModal({ coffee, onClose }) {
+  const navigate = useNavigate();
+  const { item, setItem } = useContext(CatalogStateContext);
+
+  const handleDelete = (itemId) => {
+    deleteItem(itemId);
+    const updateItems = item.filter((item) => item._id !== itemId);
+    setItem(updateItems);
+    navigate("/catalog");
+    onClose();
+  };
+
   return (
     <ThemeProvider theme={theme}>
       <Stack direction="row" spacing={2}>
@@ -18,13 +35,24 @@ function ItemModal({ coffee, onClose }) {
             ></div>
             <h1>{coffee.name}</h1>
             <p>{coffee.price}</p>
-            <Button
-              variant="outlined"
-              startIcon={<CloseIcon />}
-              onClick={onClose}
-            >
-              Close
-            </Button>
+            <div className="buttons-container">
+              <Button
+                variant="outlined"
+                startIcon={<CloseIcon />}
+                onClick={onClose}
+              >
+                Close
+              </Button>
+              <Button
+                onClick={() => {
+                  handleDelete(coffee._id);
+                }}
+                variant="outlined"
+                startIcon={<DeleteIcon />}
+              >
+                Delete
+              </Button>
+            </div>
           </div>
         </div>
       </Stack>
