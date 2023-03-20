3ef961ac094e6a0eaba215dc3373fdcbd0558d43
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2023-03-20 20:54:03 +0200

    Update CatalogItem.js

--- app/src/components/CatalogItem.js
+++ app/src/components/CatalogItem.js
@@ -3,9 +3,6 @@ import { useNavigate, Link } from "react-router-dom";
 import ItemModal from "./ItemModal";
 
 function CatalogItem({ coffee }) {
-  // let navigate = useNavigate();
-  // onClick={() => {navigate(`/details/${id}`)}}
-
   const [isModalOpen, setIsModalOpen] = useState(false);
 
   const handleItemClick = () => {
@@ -22,11 +19,8 @@ function CatalogItem({ coffee }) {
         <div style={{ backgroundImage: `url(${coffee.imageUrl})` }}></div>
         <h1> {coffee.name} </h1>
         <p> ${coffee.price} </p>
-        {/* <Link to={`/details/${coffee._id}`}>Details</Link> */}
       </div>
-      {
-        isModalOpen && <ItemModal coffee={coffee} onClose={handleCloseModal} />
-      }
+      {isModalOpen && <ItemModal coffee={coffee} onClose={handleCloseModal} />}
     </>
   );
 }
