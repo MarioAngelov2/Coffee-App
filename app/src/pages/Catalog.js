18916afe7379c4fef202c0a1f2f5111281582ad0
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2023-03-20 20:53:14 +0200

    Add new context and pass the state in order to update it when user deletes product

--- app/src/pages/Catalog.js
+++ app/src/pages/Catalog.js
@@ -1,18 +1,22 @@
 import React from "react";
 import { useState, useEffect } from "react";
 import { DataContext } from "../context/DataContext";
+import { CatalogStateContext } from "../context/DataContext";
 import { getAll } from "../services/getService";
 
-import ItemModal from "../components/ItemModal";
 import CatalogItem from "../components/CatalogItem";
 import "../styles/Catalog.css";
 
 function Catalog({}) {
   const [item, setItem] = useState([]);
 
-  const addNewItem = (newItem) => {
+  const addNewItem = async (newItem) => {
     setItem([...item, newItem]);
   };
+
+  // const deleteItem = (itemId) => {
+  //   setItem(item.filter(x => x._id !== itemId))
+  // }
 
   useEffect(() => {
     getAll().then((result) => {
@@ -21,17 +25,18 @@ function Catalog({}) {
   }, []);
 
   return (
-    <DataContext.Provider value={{ addNewItem }}>
-      <div className="menu">
-        <h1 className="catalogTitle">Our Products</h1>
-        <div className="catalogList">
-          {Object.values(item).map((item) => {
-            return <CatalogItem key={item._id} coffee={item}  />;
-          })}
+    <CatalogStateContext.Provider value={{ item: item, setItem }}>
+      <DataContext.Provider value={{ addNewItem }}>
+        <div className="menu">
+          <h1 className="catalogTitle">Our Products</h1>
+          <div className="catalogList">
+            {Object.values(item).map((item) => {
+              return <CatalogItem key={item._id} coffee={item} />;
+            })}
+          </div>
         </div>
-      
-      </div>
-    </DataContext.Provider>
+      </DataContext.Provider>
+    </CatalogStateContext.Provider>
   );
 }
 
