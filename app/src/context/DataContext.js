d6f222ab77b61c4dde9ff31b16d84cde11c6feb9
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2023-03-20 20:54:42 +0200

    Create CatalogStateContext to pass the state and use it in ItemModal

--- app/src/context/DataContext.js
+++ app/src/context/DataContext.js
@@ -1,5 +1,8 @@
+import { create } from "@mui/material/styles/createTransitions";
 import { createContext } from "react";
 
 export const DataContext = createContext({
   addNewItem: () => {}
 });
+
+export const CatalogStateContext = createContext();
