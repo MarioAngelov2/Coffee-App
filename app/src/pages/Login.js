505a4b81ce0975ed808301a17b1ccc33674b43f9
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2023-03-23 17:50:59 +0200

    Add material ui form

--- app/src/pages/Login.js
+++ app/src/pages/Login.js
@@ -1,8 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import "../styles/Login.css";
 import loginImage from "../assets/loginImage.jpeg";
 import { useNavigate } from "react-router-dom";
 import { useContext } from "react";
+
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+import LoginIcon from "@mui/icons-material/Login";
+import { ThemeProvider } from "@mui/material/styles";
+import theme from "../theme/theme";
 
 import * as authService from "../services/authService";
 import { AuthContext } from "../context/AuthContext";
@@ -16,38 +23,85 @@ function Login() {
 
     const { email, password } = Object.fromEntries(new FormData(ev.target));
 
-    authService.login(email, password)
+    authService
+      .login(email, password)
       .then((authData) => {
         userLogin(authData);
         navigate("/");
       })
-      .catch(() => {
-        navigate("/");
+      .catch((error) => {
+        alert(error);
       });
   };
 
   return (
-    <div className="loginContainer">
-      <div
-        className="leftSide"
-        style={{ backgroundImage: `url(${loginImage})` }}
-      ></div>
-      <div className="rightSide">
-        <h1>Login</h1>
-        <form id="login-form" method="POST" onSubmit={onSubmit}>
-          <label htmlFor="username">Username:</label>
-          <input name="email" placeholder="Enter username..." type="email" />
-          <label htmlFor="password">Password:</label>
-          <input
-            name="password"
-            placeholder="Enter your password..."
-            type="password"
-          />
-          <button type="submit">Login</button>
-        </form>
-      </div>
-    </div>
+    <ThemeProvider theme={theme}>
+      <Box
+        className="loginBox"
+        component="form"
+        noValidate
+        autoComplete="off"
+        onSubmit={onSubmit}
+      >
+        <div className="loginContainer">
+          <div
+            className="leftSide"
+            style={{ backgroundImage: `url(${loginImage})` }}
+          ></div>
+          <div className="rightSide">
+            <h1>Login</h1>
+            <TextField
+              id="outlined-email"
+              label="Email"
+              type="email"
+              inputProps={{ name: "email" }}
+              className="email-input"
+            />
+            <TextField
+              id="outlined-password"
+              label="Password"
+              type="password"
+              inputProps={{ name: "password" }}
+              className="password-input"
+            />
+            <Button
+              onClick={onSubmit}
+              className="login-btn"
+              type="submit"
+              variant="outlined"
+              startIcon={<LoginIcon />}
+            >
+              Login
+            </Button>
+          </div>
+        </div>
+      </Box>
+    </ThemeProvider>
   );
 }
 
 export default Login;
+
+// return (
+//   <div className="loginContainer">
+//     <div
+//       className="leftSide"
+//       style={{ backgroundImage: `url(${loginImage})` }}
+//     ></div>
+//     <div className="rightSide">
+//       <h1>Login</h1>
+//       <form id="login-form" method="POST" onSubmit={onSubmit}>
+//         <label htmlFor="username">Username:</label>
+//         <input name="email" placeholder="Enter username..." type="email" />
+//         <label htmlFor="password">Password:</label>
+//         <input
+//           name="password"
+//           placeholder="Enter your password..."
+//           type="password"
+//         />
+//         <button type="submit">Login</button>
+//       </form>
+//     </div>
+//   </div>
+// );
+// }
