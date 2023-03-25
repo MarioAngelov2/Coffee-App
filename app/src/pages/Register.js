05309393d04b9ae5418c3c43ad8b98a496ea1e06
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2023-03-25 19:06:33 +0200

    add material ui form to register component

--- app/src/pages/Register.js
+++ app/src/pages/Register.js
@@ -6,6 +6,12 @@ import { useContext } from "react";
 import { AuthContext } from "../context/AuthContext";
 import { useNavigate } from "react-router-dom";
 
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+import { ThemeProvider } from "@mui/material/styles";
+import theme from "../theme/theme";
+
 function Register() {
   const { userLogin } = useContext(AuthContext);
   const navigate = useNavigate();
@@ -18,6 +24,7 @@ function Register() {
     const email = formData.get("email");
     const password = formData.get("password");
     const repeatPass = formData.get("re-password");
+    console.log(repeatPass)
 
     if (password !== repeatPass) {
       alert("Password don't match");
@@ -30,27 +37,64 @@ function Register() {
   };
 
   return (
-    <div className="register">
-      <h1>Register</h1>
-      <form id="register-form" method="POST" onSubmit={onSubmit}>
-        <label htmlFor="username">Username:</label>
-        <input name="email" placeholder="Enter username..." type="email" />
-        <label htmlFor="password">Password:</label>
-        <input
-          name="password"
-          placeholder="Enter your password..."
-          type="password"
-        />
-        <label htmlFor="re-password">Repeat Password:</label>
-        <input
-          name="re-password"
-          placeholder="Repeat password..."
-          type="password"
-        />
-        <button type="submit">Register</button>
-      </form>
-    </div>
+    <ThemeProvider theme={theme}>
+      <div className="register">
+        <Box
+          component="form"
+          autoComplete="off"
+          className="registerBox"
+          method="POST"
+          onSubmit={onSubmit}
+        >
+          <h1>Register</h1>
+          <TextField
+            label="Email"
+            type="email"
+            className="email-input"
+            inputProps={{ name: "email" }}
+          />
+          <TextField
+            label="Password"
+            type="password"
+            className="password"
+            inputProps={{ name: "password" }}
+          />
+          <TextField
+            label="Repeat Password"
+            type="password"
+            className="re-password"
+            inputProps={{ name: "re-password" }}
+          />
+          <Button className="registerBtn" variant="contained" type="submit">
+            Sign Up
+          </Button>
+        </Box>
+      </div>
+    </ThemeProvider>
   );
 }
 
 export default Register;
+
+// {
+//   /* <div className="register">
+//       <h1>Register</h1>
+//       <form id="register-form" method="POST" onSubmit={onSubmit}>
+//         <label htmlFor="username">Username:</label>
+//         <input name="email" placeholder="Enter username..." type="email" />
+//         <label htmlFor="password">Password:</label>
+//         <input
+//           name="password"
+//           placeholder="Enter your password..."
+//           type="password"
+//         />
+//         <label htmlFor="re-password">Repeat Password:</label>
+//         <input
+//           name="re-password"
+//           placeholder="Repeat password..."
+//           type="password"
+//         />
+//         <button type="submit">Register</button>
+//       </form>
+//     </div> */
+// }
