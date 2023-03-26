55dc1b1f2ef56a127e962c94258b1f0eb564616d
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2023-03-26 23:17:58 +0300

    Reset input when user create new product

--- app/src/components/CreateItem.js
+++ app/src/components/CreateItem.js
@@ -1,11 +1,17 @@
 import { createCoffeeService } from "../services/createService";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { DataContext } from "../context/DataContext";
+import { useNavigate } from "react-router-dom";
 
 import "../styles/CreateCoffee.css";
 
 function CreateCoffee() {
   const { addNewItem } = useContext(DataContext);
+  const [inputs, setInputs] = useState({
+    imageUrl: "",
+    name: "",
+    price: "",
+  });
 
   const onSubmit = (e) => {
     e.preventDefault();
@@ -14,7 +20,16 @@ function CreateCoffee() {
 
     createCoffeeService(productData).then((result) => {
       addNewItem(result);
+      setInputs({
+        imageUrl: "",
+        name: "",
+        price: "",
+      });
     });
+  };
+
+  const handleInputChange = (e) => {
+    setInputs({ ...inputs, [e.target.name]: e.target.value });
   };
 
   return (
@@ -23,11 +38,29 @@ function CreateCoffee() {
         <div className="create-coffee-content">
           <h2>Create your coffee</h2>
           <label>Coffee Image</label>
-          <input type="url" name="imageUrl" placeholder="Paste image url..." />
+          <input
+            type="url"
+            name="imageUrl"
+            placeholder="Paste image url..."
+            value={inputs.imageUrl}
+            onChange={handleInputChange}
+          />
           <label>Coffee Name</label>
-          <input type="text" name="name" placeholder="Text coffee name..." />
+          <input
+            type="text"
+            name="name"
+            placeholder="Text coffee name..."
+            value={inputs.name}
+            onChange={handleInputChange}
+          />
           <label>Coffee Price</label>
-          <input type="text" name="price" placeholder="Coffee price..." />
+          <input
+            type="text"
+            name="price"
+            placeholder="Coffee price..."
+            value={inputs.price}
+            onChange={handleInputChange}
+          />
           <button type="submit">Create</button>
         </div>
       </form>
