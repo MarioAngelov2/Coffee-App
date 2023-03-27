020a6670bec4aac5df0d7978b8352f66e8ebfa46
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2023-03-27 23:46:48 +0300

    Add social media icons to footer

--- app/src/components/Footer.js
+++ app/src/components/Footer.js
@@ -1,12 +1,22 @@
-import React from 'react'
-import '../styles/Footer.css'
+import React from "react";
+import "../styles/Footer.css";
+import FacebookIcon from "@mui/icons-material/Facebook";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import TwitterIcon from '@mui/icons-material/Twitter';
 
 function Footer() {
   return (
-    <div className='footer'>
-      <p> &copy; 2023 Coffe Shop </p>
+    <div className="footer">
+      <p> &copy; 2023 Coffe Shop</p>
+      <div className="footer-icons">
+        <FacebookIcon />
+        <InstagramIcon />
+        <LinkedInIcon />
+        <TwitterIcon />
+      </div>
     </div>
-  )
+  );
 }
 
-export default Footer
+export default Footer;
