6d3ea797ff63616bb66549a5c1219cc163655f68
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2023-03-30 23:16:56 +0300

    Create context for shopping cart

---
+++ app/src/context/AddToCartContext.js
@@ -0,0 +1,36 @@
+import { createContext, useState, useEffect } from "react";
+import * as requester from "../services/requester";
+
+const baseUrl = "http://localhost:3030/data/cart";
+
+export const ShopContext = createContext();
+
+export const ShopContextProvider = (props) => {
+  const [cartItems, setCartItems] = useState([]);
+
+  useEffect(() => {
+    requester.get(baseUrl).then((response) => {
+      setCartItems(response);
+    });
+  }, []);
+
+  const addItemToCart = (item) => {
+    requester.post(baseUrl, item);
+
+    setCartItems([...cartItems, item]);
+  };
+
+  const removeItemFromCart = (itemId) => {
+    requester.del(`${baseUrl}/${itemId}`);
+
+    setCartItems(cartItems.filter((x) => x._id !== itemId));
+  };
+
+  return (
+    <ShopContext.Provider
+      value={{ cartItems, addItemToCart, removeItemFromCart }}
+    >
+      {props.children}
+    </ShopContext.Provider>
+  );
+};
