edaff2af478982799d150632837b9e45dc007314
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2023-03-30 23:17:12 +0300

    Minor refactor

--- app/src/pages/Home.js
+++ app/src/pages/Home.js
@@ -2,19 +2,24 @@ import React from "react";
 
 import { Link } from "react-router-dom";
 import homeImage from "../assets/coffee-banner.jpg";
+import { ThemeProvider } from "@mui/material";
+import { whiteTheme } from "../theme/theme";
+import Button from "@mui/material/Button";
 import "../styles/Home.css";
 const homeImg = `https://images.pexels.com/photos/3262277/pexels-photo-3262277.jpeg`;
 
 function Home() {
   return (
-    <div className="home" style={{ backgroundImage: `url(${homeImg})` }}>
-      <div className="headerContainer">
-        <h1>Disover the taste of real coffee</h1>
-        <Link to="/catalog">
-          <button>ORDER NOW</button>
-        </Link>
+    <ThemeProvider theme={whiteTheme}>
+      <div className="home" style={{ backgroundImage: `url(${homeImg})` }}>
+        <div className="headerContainer">
+          <h1>Disover the taste of real coffee</h1>
+          <Link to="/catalog">
+            <Button variant="outlined" className="orderBtn">Order Now</Button>
+          </Link>
+        </div>
       </div>
-    </div>
+    </ThemeProvider>
   );
 }
 
