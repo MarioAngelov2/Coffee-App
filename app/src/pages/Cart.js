19862a937d7ca80d31174725a9df763e54e9b325
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2023-04-01 16:20:19 +0300

    Conditional render items based on user

--- app/src/pages/Cart.js
+++ app/src/pages/Cart.js
@@ -1,43 +1,37 @@
 import React from "react";
 import { useContext } from "react";
 import { ShopContext } from "../context/AddToCartContext";
+import { AuthContext } from "../context/AuthContext";
 
 function Cart() {
   const { cartItems, removeItemFromCart } = useContext(ShopContext);
+  const { user } = useContext(AuthContext);
 
   const handleDeleteItem = (id) => {
     removeItemFromCart(id);
   };
 
+  const userCartItem = cartItems.filter((item) => user?._id === item._ownerId)
+
   return (
     <div>
       <h2>Cart</h2>
-    {
-      cartItems.map(x => (
-        <div key={x._id}>
-          {x.name} - {x.price}
-          <button onClick={() => handleDeleteItem(x._id)}>Delete</button>
-        </div>
-      ))
-    }
+      {user && user._id ? (
+        userCartItem.length > 0 ? (
+          userCartItem.map((x) => (
+            <div key={x._id}>
+              {x.name} - {x.price}
+              <button onClick={() => handleDeleteItem(x._id)}>Delete</button>
+            </div>
+          ))
+        ) : (
+          <div>No items in cart</div>
+        )
+      ) : (
+        <div>Please log in to view your cart</div>
+      )}
     </div>
   );
 }
 
 export default Cart;
-
-
-// {Array.isArray(cartItems) && cartItems.length > 0 ? (
-//   cartItems.map((item) => (
-//     <div key={item._id}>
-//       <p>
-//         {item.name} - {item.price}
-//       </p>
-//       <button onClick={() => handleDeleteItem(item._id)}>
-//         Remove from cart
-//       </button>
-//     </div>
-//   ))
-// ) : (
-//   <div>No items</div>
-// )}
